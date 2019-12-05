9c3af575593848b18cc94516d55e5f4f76680973
Micah Kemp <micahk@splunk.com>
2019-12-05 18:06:41 -0500

    Wait until the container is run before untarring splunk.

--- roles/docker_image_build/templates/base/entrypoint.sh
+++ roles/docker_image_build/templates/base/entrypoint.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-{{ splunk_home }}/bin/splunk start --nodaemon --accept-license --answer-yes --no-prompt
+echo Untarring splunk.tgz \
+&& tar xzf {{ splunk_home }}/splunk.tgz -C {{ splunk_home }}/.. \
+&& echo Removing splunk.tgz \
+&& rm {{ splunk_home }}/splunk.tgz \
+&& echo Starting splunk \
+&& {{ splunk_home }}/bin/splunk start --nodaemon --accept-license --answer-yes --no-prompt
