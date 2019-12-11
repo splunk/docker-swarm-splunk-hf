923b8d620bd93c935b490394003bb5e1fb153aa9
Micah Kemp <micahk@splunk.com>
2019-12-10 23:23:21 -0500

    Revert back to untarring Splunk in Dockerfile.  This can enable us to re-use that layer across multiple containers.

--- roles/docker_image_build/templates/base/entrypoint.sh
+++ roles/docker_image_build/templates/base/entrypoint.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-echo Untarring splunk.tgz \
-&& tar xzf {{ splunk_home }}/splunk.tgz -C {{ splunk_home }}/.. \
-&& echo Removing splunk.tgz \
-&& rm {{ splunk_home }}/splunk.tgz \
-&& echo Starting splunk \
-&& {{ splunk_home }}/bin/splunk start --nodaemon --accept-license --answer-yes --no-prompt
+{{ splunk_home }}/bin/splunk start --nodaemon --accept-license --answer-yes --no-prompt
