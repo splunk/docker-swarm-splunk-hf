95d79d2777c44cff0cf1a591ec39f31102a490ae
Micah Kemp <micahk@splunk.com>
2019-12-12 10:51:18 -0500

    Improved healthcheck responses.

--- roles/docker_image_build/templates/metrics_healthcheck/metrics_healthcheck.py
+++ roles/docker_image_build/templates/metrics_healthcheck/metrics_healthcheck.py
@@ -11,7 +11,7 @@ path = "{{ splunk_home }}/var/log/splunk/metrics.log*"
 
 file_names = glob.glob(path)
 if not file_names:
-    print("no files match path: {0}".format(path))
+    print("No files match path: {0}".format(path))
     exit(-1)
 
 timestamp_regex = re.compile('^(?P<timestamp>(?P<month>[0-9]{2})-(?P<day>[0-9]{2})-(?P<year>[0-9]{4}) (?P<hour>[0-9]{2}):(?P<minute>[0-9]{2}):(?P<second>[0-9]{2})\.[0-9]{3} (?P<offset_direction>[+-])(?P<offset_hours>[0-9]{2})(?P<offset_minutes>[0-9]{2})) INFO  Metrics - group=(?P<group>[^,]+), series="(?P<series>[^"]+)",')
@@ -64,19 +64,18 @@ if earliest_overall_timestamp:
         if latest_group_series_timestamp:
             latest_group_series_timestamp_epoch = int(latest_group_series_timestamp.strftime("%s"))
             latest_group_series_age = run_timestamp_epoch - latest_group_series_timestamp_epoch
+            print("{0}/{1} is {2} seconds old".format(group, series, latest_group_series_age))
             # have we seen recent data we care about?
             if latest_group_series_age > allowed_age:
-                print("bad ({0}/{1} is {2} seconds old)".format(group, series, latest_group_series_age))
                 exit(-1)
             else:
-                print("good ({0}/{1} is {2} seconds old)".format(group, series, latest_group_series_age))
                 exit(0)
         else:
-            print("bad (no logs, {0}/{1} is at least {2} seconds stale)".format(group, series, earliest_overall_age))
+            print("{0}/{1} is at least {2} seconds old (no metrics seen for this combination)".format(group, series, earliest_overall_age))
             exit(-1)
     else:
-        print("not bothering to check, we don't have {0} seconds of any group/series metrics logs".format(allowed_age))
+        print("Not bothering to check, we don't have {0} seconds of any group/series metrics logs".format(allowed_age))
         exit(0)
 else:
-    print("not bothering to check, we have no metrics logs at all!")
+    print("Not bothering to check, we have no metrics logs at all")
     exit(0)
