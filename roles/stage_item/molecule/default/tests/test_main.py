f56718222a45630a92a6e8a8b0d4ee05412dd0a6
Micah Kemp <micahk@splunk.com>
2020-02-10 16:12:12 -0500

    Migrated stage_git to stage_item.

--- roles/stage_item/molecule/default/tests/test_main.py
+++ roles/stage_item/molecule/default/tests/test_main.py
@@ -4,13 +4,12 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+).get_hosts('test_hosts')
 
 
 def test_paths(host):
     host_vars = host.ansible.get_variables()
     test_vars = host_vars['_test_config']
-
 
     paths = test_vars['paths']
 
@@ -29,12 +28,12 @@ def test_paths(host):
             print("File content")
             content = test_vars['file_contents']
             assert f.content_string == content
-            # modes are string representations of octal integers, so convert them
+            # modes are string representations of octal integers, convert them
             mode = int(test_vars['mode'], 8)
             print("File mode")
             assert f.mode == mode
         else:
             print("Directory mode")
-            # modes are string representations of octal integers, so convert them
+            # modes are string representations of octal integers, convert them
             directory_mode = int(test_vars['directory_mode'], 8)
             assert f.mode == directory_mode
