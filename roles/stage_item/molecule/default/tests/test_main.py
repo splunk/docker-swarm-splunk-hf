ee03749ba82b3067cd984fb06d20b7ad24bf5e1d
Micah Kemp <micahk@splunk.com>
2020-02-11 01:51:58 -0500

    The stage_items role now iterates through the stage_items object and calls the appropriate sub-task.  Restructured tests to be more efficient based on this new method.

--- roles/stage_item/molecule/default/tests/test_main.py
+++ roles/stage_item/molecule/default/tests/test_main.py
@@ -9,31 +9,31 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_paths(host):
     host_vars = host.ansible.get_variables()
-    test_vars = host_vars['_test_config']
+    stage_items = host_vars['stage_items']
 
-    paths = test_vars['paths']
+    for stage_item in stage_items:
+        test_config = stage_item['_test_config']
 
-    print("testing host: {0}".format(host_vars['inventory_hostname']))
-    for path in paths:
-        print("Path: {0}".format(path))
+        for path in test_config['paths']:
+            print("Path: {0}".format(path))
 
-        f = host.file(path)
+            f = host.file(path)
 
-        print("Path exists")
-        assert f.exists
-        print("Path is a file or directory")
-        assert f.is_file or f.is_directory
+            print("Path exists")
+            assert f.exists
+            print("Path is a file or directory")
+            assert f.is_file or f.is_directory
 
-        if f.is_file:
-            print("File content")
-            content = test_vars['file_contents']
-            assert f.content_string == content
-            # modes are string representations of octal integers, convert them
-            mode = int(test_vars['mode'], 8)
-            print("File mode")
-            assert f.mode == mode
-        else:
-            print("Directory mode")
-            # modes are string representations of octal integers, convert them
-            directory_mode = int(test_vars['directory_mode'], 8)
-            assert f.mode == directory_mode
+            if f.is_file:
+                print("File content")
+                content = test_config['file_contents']
+                assert f.content_string == content
+                # modes are string representations of octal integers, convert them
+                mode = int(test_config['mode'], 8)
+                print("File mode")
+                assert f.mode == mode
+            else:
+                print("Directory mode")
+                # modes are string representations of octal integers, convert them
+                directory_mode = int(test_config['directory_mode'], 8)
+                assert f.mode == directory_mode
