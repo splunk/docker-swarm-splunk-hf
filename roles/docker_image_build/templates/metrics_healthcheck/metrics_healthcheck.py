b578cd6cbfc8100523cb2d1a96e0048b8b5717b5
Micah Kemp <micahk@splunk.com>
2020-10-29 15:04:01 -0500

    #22 - Translate allowed_age_seconds to allowed_age in metrics_healthcheck.py.  Added example for multi-series healthcheck.

--- roles/docker_image_build/templates/metrics_healthcheck/metrics_healthcheck.py
+++ roles/docker_image_build/templates/metrics_healthcheck/metrics_healthcheck.py
@@ -12,13 +12,21 @@ with open(config_filename) as config_file:
 
 path = config['path']
 checks = config['checks']
+checks_normalized = [
+    {
+        "group": check["group"],
+        "series": check["series"],
+        # inventory config uses allowed_age_seconds to avoid ambiguity
+        # but the metricscheck library uses allowed_age, which represents seconds
+        "allowed_age": check["allowed_age_seconds"],
+    } for check in checks
+]
 
-metrics_checks = MetricsChecks(path, *checks)
+metrics_checks = MetricsChecks(path, *checks_normalized)
 metrics_checks.process()
 
 now = datetime.now(timezone.utc)
 
-print(list(metrics_checks.filenames()))
 print(metrics_checks.group_series_descriptions_at_time(now))
 
 if metrics_checks.any_stale_group_series_at(now):
